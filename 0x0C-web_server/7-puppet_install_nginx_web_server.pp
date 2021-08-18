52ab16d9eba8d1070747305769f8b78cff6342de
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-08-18 00:10:14 -0500

    Little fix

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,23 +1,25 @@
 # Install and configure the web server
 
 exec {'Update and Upgrade':
-  command => 'sudo apt-get -y update; sudo apt-get -y upgrade' 
+  command => 'sudo apt-get -y update; sudo apt-get -y upgrade;' 
 }
 package {'nginx':
-  ensure  => 'installed',
-  require => Exec['update packages']
+  ensure  => 'installed'
 }
 exec { 'permissions':
   command => 'sudo ufw allow "Nginx HTTP"'
 }
-service {'Start nginx':
+service {'start nginx':
   ensure  => running,
   require => Package['nginx'],
 }
 file {'/var/www/html/index.html':
   content => 'Holberton School',
 }
-exec {'Set redirection':
-  command => 'sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://github.com/jyuly12 permanent;" /etc/nginx/sites-available/default'
+file_line { 'Add redirection, 301':
+  path   => '/etc/nginx/sites-available/default',
+  ensure => 'present',
+  after  => 'listen 80 default_server',
+  line   => 'rewrite ^/redirect_me https:// permanent;'
 }
 
