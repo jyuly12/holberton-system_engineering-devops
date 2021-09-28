51cc759d54e6ddbcb177008bc0f8a71e022a0d92
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-09-27 20:39:31 -0500

    [Fix] pep8 style

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,7 +13,7 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Ana'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code == 200:
         return (response.json().get("data").get("subscribers"))
     return (0)
