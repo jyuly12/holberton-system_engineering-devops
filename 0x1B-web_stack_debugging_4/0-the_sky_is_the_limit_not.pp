bfcf0d4621772993d6da77e0a938945c0e3c9b46
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-10-07 17:50:44 -0500

    [Add]Puppet file and readme

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Change the limit on open files
+
+exec {'modify limit':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
